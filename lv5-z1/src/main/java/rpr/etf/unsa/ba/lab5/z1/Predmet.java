f89ab0fa18ffa9aae75b3e3203952ee019e5160f
Ivona <ijozic1@etf.unsa.ba>
2023-11-19 10:24:25 +0100

    Dodana klasa Predmet, kao i get i set metode za nju

---
+++ lv5-z1/src/main/java/rpr/etf/unsa/ba/lab5/z1/Predmet.java
@@ -0,0 +1,22 @@
+package rpr.etf.unsa.ba.lab5.z1;
+
+public class Predmet {
+    private String naziv, opis;
+    Predmet(){}
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public String getOpis() {
+        return opis;
+    }
+
+    public void setOpis(String opis) {
+        this.opis = opis;
+    }
+}
