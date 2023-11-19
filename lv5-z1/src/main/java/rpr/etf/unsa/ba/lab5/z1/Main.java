a14b1225b492883339988447ecb9b651040ffa7b
Ivona <ijozic1@etf.unsa.ba>
2023-11-19 10:22:13 +0100

    U Main dodane liste studenata i nastavnika

--- lv5-z1/src/main/java/rpr/etf/unsa/ba/lab5/z1/Main.java
+++ lv5-z1/src/main/java/rpr/etf/unsa/ba/lab5/z1/Main.java
@@ -1,12 +1,20 @@
 package rpr.etf.unsa.ba.lab5.z1;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+        List<InformacijeOStudentu> studenti= new ArrayList<>();
+        List<InformacijeONastavniku> nastavnici=new ArrayList<>();
+
         InformacijeOStudentu s1= new InformacijeOStudentu("Ivona", "Jozic", "2","19357");
+        studenti.add(s1);
         InformacijeOStudentu s2=new InformacijeOStudentu("Ismar", "Muslic","2","19304");
+        studenti.add(s2);
         InformacijeONastavniku n1=new InformacijeONastavniku("Hamza", "Iseric", "strucnjak iz prakse");
+        nastavnici.add(n1);
 
         System.out.println("S1: "+s1.getIme()+" "+s1.getPrezime()+", broj indeksa "+s1.getBrIndexa());
         System.out.println("S2: "+s2.getIme()+" "+s2.getPrezime()+", broj indeksa "+s2.getBrIndexa());
@@ -37,6 +45,7 @@ public class Main {
                             brIndeksa=ulaz.nextLine();
                             InformacijeOStudentu s= new InformacijeOStudentu(ime, prezime, godStudija, brIndeksa);
                             System.out.println("Student: "+s.getIme()+" "+s.getPrezime()+", broj indeksa "+s.getBrIndexa());
+                            studenti.add(s);
                             break;
                         case 2:
                             ulaz.nextLine(); 
@@ -49,6 +58,7 @@ public class Main {
                             titula=ulaz.nextLine();
                             InformacijeONastavniku n= new InformacijeONastavniku(imeN, prezimeN, titula);
                             System.out.println("Nastavnik: "+n.getIme()+" "+n.getPrezime()+", titula: "+n.getTitula());
+                            nastavnici.add(n);
                             break;
                         default:
                             System.out.println("Pogresan unos!");
@@ -60,6 +70,14 @@ public class Main {
                 default:
                     System.out.println("Pogresan unos! Pokusajte ponovno: ");
             }
+        }
+        System.out.println("\nSvi uneseni studenti: ");
+        for(InformacijeOStudentu s : studenti){
+            System.out.println(s.getIme()+" "+s.getPrezime()+", broj indeksa "+s.getBrIndexa());
+        }
+        System.out.println("\nSvi unesesni nastavnici: ");
+        for(InformacijeONastavniku nast : nastavnici){
+            System.out.println(nast.getIme()+" "+nast.getPrezime()+", titula: "+nast.getTitula());
         }
     }
 }
