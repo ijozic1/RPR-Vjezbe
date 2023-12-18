5d28b93d30ac096bdf978bcf8c3c5714378427c8
Ivona <ijozic1@etf.unsa.ba>
2023-12-19 00:14:13 +0100

    Zavrsene postavke GUI

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -2,9 +2,19 @@ package com.example.demo;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class HelloController {
+    public Button krajDugme;
+    public Button dodajDugme;
+    public PasswordField lozinkaPolje;
+    public TextField imePolje;
+    public TextField prezimePolje;
+    public TextField eMailPolje;
+    public TextField usernamePolje;
     @FXML
     private Label welcomeText;
 
