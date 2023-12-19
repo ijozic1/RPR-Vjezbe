0cfb485f06d31b3e402eb9c279c81cec85e69c02
Ivona <ijozic1@etf.unsa.ba>
2023-12-19 19:54:57 +0100

    Dorada klase LoginApplication

--- demo/src/main/java/com/example/demo/LoginApplication.java
+++ demo/src/main/java/com/example/demo/LoginApplication.java
@@ -4,16 +4,26 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 import java.io.IOException;
 
 public class LoginApplication extends Application {
+    private ObservableList<Korisnik> ucitajPodatke(){
+        ObservableList<Korisnik> lista=FXCollections.observableArrayList();
+        lista.add(new Korisnik("Ivona", "Jozic", "ijozic1@etf.unsa.ba","ijozic1","1234"));
+        lista.add(new Korisnik("Ismar", "Muslic", "imuslic1@etf.unsa.ba", "imuslic1", "5678"));
+        return lista;
+    }
     @Override
     public void start(Stage stage) throws IOException {
         KorisniciModel model = new KorisniciModel();
-        model.napuni();
+        model.setKorisnici(ucitajPodatke());
+
         FXMLLoader fxmlLoader = new FXMLLoader(LoginApplication.class.getResource("hello-view.fxml"));
         fxmlLoader.setController(new LoginController(model));
+
         Scene scene = new Scene(fxmlLoader.load(), 398, 288);
         stage.setTitle("Login forma");
         stage.setScene(scene);
