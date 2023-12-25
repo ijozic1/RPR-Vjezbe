37cb7f60f8d19614745998a680d7ae7ab13f228e
Ivona <ijozic1@etf.unsa.ba>
2023-12-25 23:34:15 +0100

    Main klasa

--- lv9-z1/src/main/java/rpr/etf/unsa/ba/lab9/z1/Main.java
+++ lv9-z1/src/main/java/rpr/etf/unsa/ba/lab9/z1/Main.java
@@ -1,8 +1,51 @@
 package rpr.etf.unsa.ba.lab9.z1;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Main {
+    private GeografijaDAO dao=GeografijaDAO.getInstance();
+
+    Main(){}
+    String ispisiGradove(){
+        String ispis = "";
+        try {
+            ArrayList<Grad> gradovi = dao.gradovi();
+            for (Grad g : gradovi) {
+                String s = g.getNaziv() + " (" + g.getImeDrzave() + ") - " + g.getBroj_stanovnika() + "\n";
+                ispis += s;
+            }
+        }
+        catch(NullPointerException e){
+            ispis=e.getMessage();
+        }
+        return ispis;
+    }
+    void glavniGrad(){
+        Scanner unos=new Scanner(System.in);
+        System.out.print("Unesite naziv drzave za koju trazite glavni grad: ");
+        String naziv=unos.nextLine();
+        Drzava d= dao.nadjiDrzavu(naziv);
+        Grad glavni=dao.glavniGrad(d.getNaziv());
+        if(glavni==null){
+            System.out.println("Nepostojeca drzava");
+        }
+        else{
+            System.out.println("Glavni grad drzave "+naziv+" je "+glavni.getNaziv());
+        }
+    }
+
+    public static void main(String[] args){
+        Main m=new Main();
+        System.out.println("Ako zelite izlistati postojece gradove pritisnite 1: ");
+        Scanner unos=new Scanner(System.in);
+        int broj =unos.nextInt();
+        if(broj==1) {
+            String ispis=m.ispisiGradove();
+            System.out.println(ispis);
+        }
+        System.out.println("Ako zelite pretraziti neku drzavu unesite 1: ");
+        broj=unos.nextInt();
+        if(broj==1) m.glavniGrad();
     }
 }
