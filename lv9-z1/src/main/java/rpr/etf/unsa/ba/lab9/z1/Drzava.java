3f6ac894dc737d09bc8aa6738d5203e07b47501c
Ivona <ijozic1@etf.unsa.ba>
2023-12-25 21:15:37 +0100

    klasa Drzava - dorada

--- lv9-z1/src/main/java/rpr/etf/unsa/ba/lab9/z1/Drzava.java
+++ lv9-z1/src/main/java/rpr/etf/unsa/ba/lab9/z1/Drzava.java
@@ -2,13 +2,14 @@ package rpr.etf.unsa.ba.lab9.z1;
 
 public class Drzava {
     private String naziv;
-    private int id;
+    private int id, glavni_grad;
 
     public Drzava(){}
 
-    public Drzava(int id, String naziv){
+    public Drzava(int id, String naziv, int glvGrad){
         this.id=id;
         this.naziv=naziv;
+        this.glavni_grad=glvGrad;
     }
 
     public String getNaziv() {
@@ -25,5 +26,13 @@ public class Drzava {
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public int getGlavni_grad() {
+        return glavni_grad;
+    }
+
+    public void setGlavni_grad(int glavni_grad) {
+        this.glavni_grad = glavni_grad;
     }
 }
