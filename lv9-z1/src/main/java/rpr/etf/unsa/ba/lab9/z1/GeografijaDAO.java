c66b0ff595057236a8c56dad77c6093afb474de8
Ivona <ijozic1@etf.unsa.ba>
2023-12-25 22:01:06 +0100

    klasa GeografijaDAO za 2. zadatak

--- lv9-z1/src/main/java/rpr/etf/unsa/ba/lab9/z1/GeografijaDAO.java
+++ lv9-z1/src/main/java/rpr/etf/unsa/ba/lab9/z1/GeografijaDAO.java
@@ -6,12 +6,69 @@ import java.util.ArrayList;
 public class GeografijaDAO {
     private static GeografijaDAO instance=null;
     private Connection conn;
-    private PreparedStatement dajGlavniGrad, dajDrzavu, dajBrojStanovnika, dajGradove;
+    private PreparedStatement dajGlavniGrad, dajDrzavu, dajBrojStanovnika, dajGradove, kreirajGrad, kreirajDrzavu, alterGrad, upitSviGradovi;
+
+    private void stvoriBazu(){
+        try{
+            kreirajGrad.executeQuery();
+            kreirajDrzavu.executeQuery();
+            alterGrad.executeQuery();
+
+            String upitG1="insert into grad(id,naziv,broj_stanovnika, drzava) " +
+                        "values ((select max(id) from grad)+1,'London',8539000, 1);";
+            String upitD1="insert into drzava(id, naziv, glavni_grad) " +
+                    "values((select max(id) from drzava)+1,'Ujedinjeno Kraljevstvo',1);";
+            Statement stmt= conn.createStatement();
+            stmt.execute(upitG1);
+            stmt.execute(upitD1);
+
+
+            upitG1="insert into grad(id,naziv,broj_stanovnika, drzava) " +
+                    "values ((select max(id) from grad)+1,'Pariz',2161000, 2);";
+            upitD1="insert into drzava(id, naziv, glavni_grad) " +
+                    "values((select max(id) from drzava)+1,'Francuska',2);";
+            stmt.execute(upitG1);
+            stmt.execute(upitD1);
+
+            upitG1="insert into grad(id,naziv,broj_stanovnika, drzava) " +
+                    "values ((select max(id) from grad)+1,'Bec',1800000, 3);";
+            upitD1="insert into drzava(id, naziv, glavni_grad) " +
+                    "values((select max(id) from drzava)+1,'Austrija',3);";
+            stmt.execute(upitG1);
+            stmt.execute(upitD1);
+
+            upitG1="insert into grad(id,naziv,broj_stanovnika, drzava) " +
+                    "values ((select max(id) from grad)+1,'Graz',280800, 3);";
+            stmt.execute(upitG1);
+
+            upitG1="insert into grad(id,naziv,broj_stanovnika, drzava) " +
+                    "values ((select max(id) from grad)+1,'Mancester',520215, 1);";
+        }
+        catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+
+    }
+    private void provjeriKonekciju(){
+        try{
+            upitSviGradovi=conn.prepareStatement("Select * from grad;");
+        }
+        catch(SQLException e){
+            stvoriBazu();
+            try{
+               upitSviGradovi=conn.prepareStatement("Select * from grad");
+            }
+            catch(SQLException e1){
+                System.out.println(e1.getMessage());
+            }
+        }
+    }
 
     private GeografijaDAO() {
         String url="jdbc:sqlite:identifier.sqlite";
         try{
             conn= DriverManager.getConnection(url);
+            provjeriKonekciju();
             dajGlavniGrad=conn.prepareStatement("SELECT grad.naziv" +
                                                     "from grad, drzava" +
                                                     "where grad.id=drzava.glavni_grad and lower(drzava.naziv)=lower('?');");
@@ -24,6 +81,9 @@ public class GeografijaDAO {
             dajGradove=conn.prepareStatement("select id, naziv, broj_stanovnika, drzava"+
                                                  "from grad" +
                                                  "order by broj_stanovnika desc;");
+            kreirajDrzavu=conn.prepareStatement("create table grad(id number primary key, naziv varchar2, broj_stanovnika number);");
+            kreirajGrad=conn.prepareStatement("create table drzava(id number primary key ,naziv varchar2,glavni_grad number references grad(id));");
+            alterGrad=conn.prepareStatement("alter table grad add drzava number references drzava(id);");
         }
         catch(SQLException e){
             System.out.println(e.getMessage());
